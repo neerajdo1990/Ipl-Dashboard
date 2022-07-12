2495e34a69b93e78bad4ed91628611cc535db48d
neerajdo1990 <neeraj.dorsatwar.1990@gmail.com>
2022-07-12 22:02:11 +0530

    updated batch code
    Updated batch code to create team table and add data in it.

--- src/main/java/com/ndtech/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/com/ndtech/ipldashboard/data/JobCompletionNotificationListener.java
@@ -1,35 +1,66 @@
 package com.ndtech.ipldashboard.data;
 
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import com.ndtech.ipldashboard.model.Team;
 
 	@Component
 	public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
 	  private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-	  private final JdbcTemplate jdbcTemplate;
+	  private final EntityManager em;
 
 	  @Autowired
-	  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-	    this.jdbcTemplate = jdbcTemplate;
+	  public JobCompletionNotificationListener(EntityManager em) {
+	    this.em = em;
 	  }
 
 	  @Override
+	  @Transactional
 	  public void afterJob(JobExecution jobExecution) {
 	    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
 	      log.info("!!! JOB FINISHED! Time to verify the results");
+	      
+	      Map<String, Team> teamData = em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+	      .getResultList()
+	      .stream()
+	      .map(x-> new Team((String)x[0], (long)x[1]))
+	      .collect(Collectors.toMap(x->x.getTeamName(), x->x));
+	      
+	      em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+	    	      .getResultList()
+	    	      .stream()
+	    	      .forEach(x->{
+	    	    	  Team team = teamData.get((String)x[0]);
+	    	    	  team.setTotalMatches(team.getTotalMatches()+(long)x[1]);
+	    	      });
+	      
+	      em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+	      .getResultList()
+	      .stream()
+	      .forEach(x->{
+	    	  Team team = teamData.get((String)x[0]);
+	    	  if(team!=null)
+	    		  team.setTotalWins((long)x[1]);
+	      });
+	      
+	      teamData.values().forEach(team-> em.persist(team));
+	      teamData.values().forEach(team-> System.out.println(team));
 
-	      jdbcTemplate.query("SELECT team1, team2, date FROM match",
-	        (rs, row) -> "Team 1 " + rs.getString(1) + " Team 2 " + rs.getString(2) + " Date "+ rs.getString(3)
-	      ).forEach(x -> log.info("Found <" + x + "> in the database."));
 	    }
 	  }
 	}
